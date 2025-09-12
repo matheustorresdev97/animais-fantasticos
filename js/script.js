54a392cb060719205c077ed22adf790cc3751e51
matheustorresdev97 <matheustorresdev@gmail.com>
2025-09-12 12:50:55 -0300

    tabnav

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
+import TabNav from "./modules/tabnav.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
+
+const tabNav = new TabNav(
+  '[data-tab="menu"] li',
+  '[data-tab="content"] section'
+);
+tabNav.init();
